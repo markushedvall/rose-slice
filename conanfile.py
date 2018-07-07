5ed23501309b14a2cfdc152a024224facec2a26a
Markus Hedvall <mackanhedvall@gmail.com>
2018-07-07 21:34:16 +0800

    v0.1.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class RoseSliceConan(ConanFile):
     name = "rose-slice"
-    version = "0.0.0"
+    version = "0.1.0"
     license = "MIT OR Apache-2.0"
     url = "https://github.com/markushedvall/rose-slice"
     description = "Single header C++ library for memory slices"
